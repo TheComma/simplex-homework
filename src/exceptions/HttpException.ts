f38f9b15077117e160455aaf2d849a7a68171b8d
Tadas <tadas.radvilavicius@telesoftas.com>
2022-09-13 22:45:40 +0300

    refactor: edit exception to provide result message

--- src/exceptions/HttpException.ts
+++ src/exceptions/HttpException.ts
@@ -3,10 +3,12 @@ import { HttpError } from 'routing-controllers';
 export class HttpException extends HttpError {
   public status: number;
   public message: string;
+  public resultMessage?: string;
 
-  constructor(status: number, message: string) {
+  constructor(status: number, message: string, resultMessage?: string) {
     super(status, message);
     this.status = status;
     this.message = message;
+    this.resultMessage = resultMessage;
   }
 }
