19a97ed28cb51ba4f3870d5f6e17142f5acb4bc4
Tadas <tadas.radvilavicius@telesoftas.com>
2022-09-16 10:23:12 +0300

    refactor: add validation for env parameters

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,9 +1,12 @@
-import { cleanEnv, port, str } from 'envalid';
+import { cleanEnv, port, str, num, url } from 'envalid';
 
 const validateEnv = () => {
   cleanEnv(process.env, {
     NODE_ENV: str(),
     PORT: port(),
+    CACHE_MAX_SIZE: num(),
+    EXCHANGE_RATE_API_URL: url(),
+    TTL_FOR_CACHE_RECORD: num(),
   });
 };
 
